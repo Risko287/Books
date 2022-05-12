37a56636347beb33d938607bd8534fd61e1c6c0f
Richard <richard.spurnay@gmail.com>
2022-05-12 13:47:07 +0200

    fix post lending list

--- src/main/java/sk/stuba/fei/uim/oop/assignment3/list/IListService.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/list/IListService.java
@@ -3,7 +3,7 @@ package sk.stuba.fei.uim.oop.assignment3.list;
 public interface IListService {
 
     java.util.List<List> getAllLists();
-    List createList(ListRequest request);
+    List createList();
     List getListById(Long id);
     void deleteListById(Long id);
 
